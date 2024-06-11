394202d3d8d1c5f56595081cb370ebc18f16182f
Miroslav Jonas <missing.manual@gmail.com>
2024-06-11 13:58:47 +0200

    switch on ci

--- application/src/main/java/com/example/multimodule/application/DemoApplication.java
+++ application/src/main/java/com/example/multimodule/application/DemoApplication.java
@@ -18,7 +18,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String home() {
-		return myService.message() + "changed";
+		return myService.message() + "some message";
 	}
 
 	public static void main(String[] args) {
